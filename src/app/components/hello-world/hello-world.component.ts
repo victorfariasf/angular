8330710016f44d116bd10e73300a59a6b2e24993
Victor <victorfariasfer@gmail.com>
2025-04-10 15:53:15 -0300

    first commit

---
+++ src/app/components/hello-world/hello-world.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.sass']
+})
+export class HelloWorldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
